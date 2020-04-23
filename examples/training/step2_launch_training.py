96b1dcf348c36c724fb439f223ed27bca1050ebe
emoebel <emmanuel.moebel@inria.fr>
2020-04-23 11:41:48 +0200

    corrected examples/launch_training.py: dim_in is now parameter of obj constructor

--- examples/training/step2_launch_training.py
+++ examples/training/step2_launch_training.py
@@ -20,12 +20,12 @@ path_objl_train = 'in/object_list_train.xml'
 path_objl_valid = 'in/object_list_valid.xml'
 
 Nclass = 13
+dim_in = 56 # patch size
 
 # Initialize training task:
-trainer = Train(Ncl=Nclass)
+trainer = Train(Ncl=Nclass, dim_in=dim_in)
 trainer.path_out         = 'out/' # output path
 trainer.h5_dset_name     = 'dataset' # if training data is stored as h5, you can specify the h5 dataset
-trainer.dim_in           = 56 # patch size
 trainer.batch_size       = 25
 trainer.epochs           = 100
 trainer.steps_per_epoch  = 100
@@ -33,6 +33,10 @@ trainer.Nvalid           = 1 # steps per validation
 trainer.flag_direct_read     = False
 trainer.flag_batch_bootstrap = True
 trainer.Lrnd             = 13 # random shifts when sampling patches (data augmentation)
+trainer.class_weights = None # keras syntax: class_weights={0:1., 1:10.} every instance of class 1 is treated as 10 instances of class 0
+
+# Use following line if you want to resume a previous training session:
+#trainer.net.load_weights('out/round1/net_weights_FINAL.h5')
 
 # Load object lists:
 objl_train = ol.read_xml(path_objl_train)
