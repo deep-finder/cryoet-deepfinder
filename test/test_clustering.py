3061b81824eb11f36533ea1b91e3d85180152864
emoebel <emmanuel.moebel@inria.fr>
2023-11-07 18:20:27 +0100

    added 1st version of unit test for clustering

---
+++ test/test_clustering.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+import shutil, tempfile
+import numpy as np
+
+from deepfinder.training import TargetBuilder
+from deepfinder.inference import Cluster
+from deepfinder.utils.eval import Evaluator
+
+from test_utils import create_dummy_objl
+
+
+class TestClustering(unittest.TestCase):
+
+    def test_clustering(self):
+        cradius = 3
+        tomodim = (50, 100, 100)
+
+        # Get dummy objl:
+        objl_true = create_dummy_objl(n_obj=20, n_obj_classes=2, tomodim=tomodim)  # TODO: replace with exact positions cause right now there is a risk of overlapping objects
+
+        # Create dummy label map:
+        tbuild = TargetBuilder()
+        lmap = tbuild.generate_with_spheres(
+            objl=objl_true,
+            target_array=np.zeros(tomodim),
+            radius_list=[cradius, cradius]
+        )
+
+        # Initialize clustering task:
+        clust = Cluster(clustRadius=cradius)
+
+        # Launch clustering:
+        objl_pred = clust.launch(lmap)
+
+        # Eval:
+        dset_true = {'tomo0': {'object_list': objl_true}}
+        dset_pred = {'tomo0': {'object_list': objl_pred}}
+        eval = Evaluator(dset_true, dset_pred, dist_thr=2).get_evaluation(score_thr=None)
+
+        self.assertEqual(eval['global']['f1s']['1'], 1)  # f1 scores for all classes should be =1
+        self.assertEqual(eval['global']['f1s']['2'], 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
